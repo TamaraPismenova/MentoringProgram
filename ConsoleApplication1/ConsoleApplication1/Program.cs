aac9705e7b56b5809a67a964299428928aae9612
unknown <Tamara_Pismenova@EPUAKYIW2372.kyiv.epam.com>
2017-03-23 20:00:14 +0200

    test comment

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Please, enter your name> ");
+            var myName = Console.ReadLine();
+
+            Console.WriteLine("\n\rHello " + myName+"!\n\r");
+
+            Console.Write("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
